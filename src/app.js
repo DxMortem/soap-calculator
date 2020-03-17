b284b3cbfb06f86d52ab4c460d61bee516205313
Diego Borrero <diego.borrero@payulatam.com>
2020-03-16 22:22:37 -0500

    add handler for 404

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const index = require('./routes/index.controller');
+const exceptionHandler = require('./exception-handler');
 const app = express();
 
 app.use('/', index);
 app.use(express.static('public'));
+app.use('/*', exceptionHandler);
 
 module.exports = app;
---
+++ src/exception-handler.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res, next) => {
+    res.status(404).send('<h1>Error 404: Not Found</h1>')
+});
+
+module.exports = router;
