b51a925d2005ddddf7cddfd7db9856fe961f2c1b
Diego Borrero <diego.borrero@payulatam.com>
2020-03-16 22:21:41 -0500

    added index html for description

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
+const index = require('./routes/index.controller');
 const app = express();
+
+app.use('/', index);
+app.use(express.static('public'));
 
 module.exports = app;
