d6cf955af786c49c74a136e19fc0f1511fad340a
Diego Borrero <diego.borrero@payulatam.com>
2020-03-19 18:42:41 -0500

    removing unnecesary parameter

--- src/exception-handler.js
+++ src/exception-handler.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.status(404).send('<h1>Error 404: Not Found</h1>');
 });
 
--- src/routes/index.controller.js
+++ src/routes/index.controller.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
