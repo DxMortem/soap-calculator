a0a4ef72cf272b1f5021a4a6918848b235e380dc
Diego Borrero <diego.borrero@payulatam.com>
2020-03-20 01:02:10 -0500

    added logs and refactoring

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = require('./app');
 const config = require('./common/configuration');
 const calculator = require('./routes/calc.controller');
+const logger = require('./common/logger')(module);
 
 const port = config.get('app.port');
 const soapPath = config.get('soap.path');
@@ -10,6 +11,6 @@ const wsdlPath = config.get('soap.wsdlFilePath');
 const wsdl = fs.readFileSync(process.cwd() + wsdlPath, 'utf8');
 
 app.listen(port, () => {
-  soap.listen(app, soapPath, calculator.service, wsdl, () => { console.log('SOAP Initialized'); });
-  console.log('Server running at port:', port);
+  soap.listen(app, soapPath, calculator.service, wsdl, () => { logger.info('SOAP Initialized'); });
+  logger.info(`Server running at port: ${port}`);
 });
